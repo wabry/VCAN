e445db09dd5b6ffdbc37cbc0b8aff4dc50567c7c
boyercd <boyercd@umich.edu>
2018-03-06 09:33:13 -0500

    foreign key fix and favorites isolation

--- webserver/data.sql
+++ webserver/data.sql
@@ -2,4 +2,4 @@ INSERT INTO folders(ID,name,parentName,favorited,static)
 VALUES
 (0,'root','root',NULL,1),
 (1,'downloads','root',NULL,1),
-(2,'favorites','root',NULL,1);
+(2,'favorites','favorites',NULL,1);

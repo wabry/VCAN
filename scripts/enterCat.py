6fa35e77fa8f024fd9e8e2aae3132b18de94cd0e
wabryant <wabryant@umich.edu>
2018-03-20 01:06:46 -0400

    Ready for Beta

--- scripts/enterCat.py
+++ scripts/enterCat.py
@@ -1,6 +1,6 @@
 import sys, requests
 catIndex = sys.argv[1]
 payload = {'category' : catIndex}
-url = 'http://localhost:8000/api/alexa/v1/appStore/' + catIndex
+url = 'http://localhost:8000/api/alexa/v1/appStore/category/' + catIndex
 r = requests.post(url, payload)
 print(r)

03406d30e358b7b553a10825548259408c760f9b
boyercd <boyercd@umich.edu>
2018-04-17 09:38:10 -0400

    scraper fix

--- vcan-v1/scraper/categories.py
+++ vcan-v1/scraper/categories.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 url = "https://www.amazon.com/alexa-skills/b/ref=topnav_storetab_a2s?ie=UTF8&node=13727921011"
 temp = []
 count = 0
-attempts = 1 
+attempts = 5 
 while attempts > 0:
     try:
         r = requests.get(url, headers = {'User-agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0"})

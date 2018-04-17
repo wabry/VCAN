46f2672c07c89ccec54cf8d7d2e1a6790e52322e
boyercd <boyercd@umich.edu>
2018-04-17 09:35:43 -0400

    scraper fix

--- vcan-v1/scraper/categories.py
+++ vcan-v1/scraper/categories.py
@@ -4,10 +4,10 @@ from bs4 import BeautifulSoup
 url = "https://www.amazon.com/alexa-skills/b/ref=topnav_storetab_a2s?ie=UTF8&node=13727921011"
 temp = []
 count = 0
-attempts = 5 
+attempts = 1 
 while attempts > 0:
     try:
-        r = requests.get(url)
+        r = requests.get(url, headers = {'User-agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0"})
         #print(r.text)
         soup = BeautifulSoup(r.content, "html.parser")
         for li in soup.find(attrs={'aria-label': "Categories and Refinements"}).find(text="Alexa Skills").next.next:
--- vcan-v1/scraper/skill_info.py
+++ vcan-v1/scraper/skill_info.py
@@ -6,9 +6,9 @@ from bs4 import BeautifulSoup
 def get_json(url):
     #url = 'https://www.amazon.com/s/ref=lp_13727921011_nr_n_1?fst=as%3Aoff&rh=n%3A13727921011%2Cn%3A%2113727922011%2Cn%3A14284865011&bbn=13727922011&ie=UTF8&qid=1520952570&rnid=13727922011'
     check = 0
-
+    url = "https://www.amazon.com" + url
     while(check == 0):
-        totalpage = requests.get(url)
+        totalpage = requests.get(url, headers = {'User-agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0"})
         #print(totalpage.text[:500])
         soup = BeautifulSoup(totalpage.text, 'html.parser')
         skills_containers = soup.find_all('div',class_ = 's-item-container')
